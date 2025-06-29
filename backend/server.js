c2ac6255cf4d9ba4379232937cad9171289a1094
rejoice144 <rejoiceraphael14@gmail.com>
2025-06-29 15:16:17 +0100

    job page create and   api created   , json file create

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+// Main Express Application
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path =require('path')
+const app = express();
+const PORT = 3000;
+ // middleware
+ app.use(cors());
+ app.use(bodyParser.json());
+ app.use(express.static(path.join(__dirname,'../frontend')));
+// Import routers
+const jobsRouter = require('./routes/jobs');
+
+
+
+// use routes
+app.use('/api/jobs',jobsRouter);
+app.use('/api/clients',clientsRouter);
+app.use('/api/materials',materialsRouter);
+
+// server frontend
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../frontend/index.html'));
+});
+// start server
+app.listen(PORT,()=>{
+    console.log(`server running on http://localhost:${PORT}`);
+});
+
+
